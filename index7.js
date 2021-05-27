dd272c7893ee54e8291399cc9483d14d82919c88
TroungRee <leee4216@gmail.com>
2021-05-27 10:52:43 -0700

    s

--- index7.js
+++ index7.js
@@ -1,21 +1,49 @@
-let express = require('express');
-var bodyParser = require('body-parser');
-var routes = require("./routes");   //new
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var express = require("express");
+var flash = require("connect-flash");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var path = require("path");
+var session = require("express-session");
 
-let app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+
+var Info = require("./models/Info");   //maybe not needed.
+
+var setUpPassport = require("./setuppassport");
+var routes = require("./routes");
+var routesData = require("./routesData");    //added
+
+var app = express();
+mongoose.connect("mongodb://localhost:27017/userdb");   //27017 seems to be the port number used by mongod
+setUpPassport();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use('/', express.static('./'));
+app.use('/js', express.static('./public/js'));
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', express.static('./'));   //new
-app.use('/js', express.static('./public/js'));   //new
-app.use(routes);  //new
+app.use(cookieParser());
 
+app.use(session({
+  secret: "LUp$Dg?,I#i&owP3=9su+OB%`JgL4muLF5YJ~{;t",
+  resave: true,
+  saveUninitialized: true
+}));
 
-//app.use('/', express.static('./'));   //new
+app.use(flash());
 
-//req is info sending to server from client.
-//res is info sending to client from server.
+app.use(passport.initialize());
+app.use(passport.session());
 
-  console.log("Hi");
-app.listen(4007, function () {
-  console.log('Example app listening on port 4007!');
+app.use(routes);
+app.use(routesData);
+
+app.listen(app.get("port"), function() {
+  console.log("Server started on port " + app.get("port"));
 });
--- routes.js
+++ routes.js
@@ -1,17 +1,18 @@
+var path = require("path");
 var express = require("express");
+var passport = require("passport");
+
+var Info = require("./models/Info");
+var User = require("./models/user");
+
 var router = express.Router();
-var formidable = require('formidable');
-var mv = require('mv');
+const Recipe = require('./Recipe');
 
 
-
-
-//var addRecipe = require("/public/js/addRecipe.js");
-//console.log(addRecipe);
 router.get("/",function(req,res){
-    //addRecipe.testing();
-
-    res.sendFile(__dirname + "/public/html/home.html");
+    console.log("get root");
+    let thePath = path.resolve(__dirname,"public/views/home.html");
+    res.sendFile(thePath);
 });
 
 router.get("/home",function(req,res){
@@ -30,7 +31,91 @@ router.get("/show",function(req,res){
 		res.sendFile(__dirname + "/public/html/displayRecipe.html");
 });
 
+router.get("/signup", function(req, res) {
+    console.log("get signup");
+    initIdent();
 
+  	let thePath = path.resolve(__dirname,"public/views/signup.html");
+  	res.sendFile(thePath);
+});
+
+router.get("/login", function(req, res) {
+    console.log("get login");
+
+  	let thePath = path.resolve(__dirname,"public/views/login.html");
+  	res.sendFile(thePath);
+});
+
+router.get("/session", function(req, res) {
+    console.log("get session");
+    if (req.isAuthenticated()) {
+        if (req.user.username == "admin") {
+            let thePath = path.resolve(__dirname,"public/views/adminsession.html");
+            res.sendFile(thePath);
+        }
+        else {
+    	     let thePath = path.resolve(__dirname,"public/views/session.html");
+    	     res.sendFile(thePath);
+        }
+    }
+    else {
+        let thePath = path.resolve(__dirname,"public/views/login.html");
+        res.sendFile(thePath);
+    }
+});
+
+router.get("/adminInfo",function(req,res){
+    if (req.isAuthenticated()) {
+        if (req.user.username == "admin"){
+            initAdmin(req,res);
+        }
+        else
+            res.json(null);
+    }
+    else {
+        res.json(null);
+    }
+});
+
+function initAdmin(req,res) {
+    console.log("initAdmin");
+    console.log(req.user.ident);
+    console.log(req.user.username);
+
+    Info.find({},function(error,info) {
+        if (error) {
+          return res.json(null);
+        }
+        else {
+            let list = [];
+            for (let i=0;i<info.length;i++) {
+                list.push({ident:info[i].ident,name:info[i].name});
+            }
+            res.json ({ ident:req.user.ident,username: req.user.username,userList:list});
+        }
+    });
+}
+
+router.get("/userInfo",function(req,res){
+    console.log("top userInfo");
+    if (req.isAuthenticated()) {
+        console.log("userInfo is auth");
+        db.getStudent(req.user.ident,res);
+    }
+    else {
+        res.json(null);
+    }
+});
+
+router.get("/logout", function(req, res) {
+    if (req.isAuthenticated()) {
+        req.logout();
+        res.redirect("/successroot");
+    }
+    else {
+        res.redirect("/failroot");
+    }
+});
 
 const Recipe = require('./Recipe');
 const myDatabase = require('./myDatabase');
@@ -39,7 +124,38 @@ let serverDb = new myDatabase();
 var recipeInfo = {};
 
 
+router.post("/signup", function(req, res, next) {
+    console.log("post signup");
 
+    var username = req.body.username;
+    var password = req.body.password;
+    ident++;
+
+    User.findOne({ username: username }, function(err, user) {
+        if (err) { return next(err); }
+        if (user) {
+          req.flash("error", "User already exists");
+          return res.redirect("/failsignup");
+        }
+
+        var newUser = new User({
+          username: username,
+          password: password,
+          ident: ident
+        });
+        newUser.save(next);    //this line has to be called.
+    });
+    }, passport.authenticate("login", {
+        successRedirect: "/successsignup",
+        failureRedirect: "/failsignup",
+        failureFlash: true
+}));
+
+router.post("/login", passport.authenticate("login", {
+  successRedirect: "/successlogin",
+  failureRedirect: "/faillogin",
+  failureFlash: true
+}));
 
 //server
 router.post('/serverCreate', function(req, res){
